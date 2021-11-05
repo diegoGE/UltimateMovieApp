51c9e36f2ff999b0a34c9da10393d1cd5a8dd8f6
PCSMEXICO <estradadg7@gmail.com>
2021-11-05 15:29:40 -0600

    Activities y Lifecycles

--- app/src/main/java/com/example/ultimatemovieapp/MainActivity.kt
+++ app/src/main/java/com/example/ultimatemovieapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ultimatemovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val texto = findViewById<TextView>(R.id.txt_output)
+        val boton = findViewById<Button>(R.id.btn_accion)
+
+        boton.setOnClickListener {
+            texto.text = "Curso Android"
+        }
     }
 }
