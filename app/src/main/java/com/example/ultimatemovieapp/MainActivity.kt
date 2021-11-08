275b44ac59d90505a92d864111393ec4d60b0738
PCSMEXICO <estradadg7@gmail.com>
2021-11-08 16:58:42 -0600

    Pasar datos entre actividades

--- app/src/main/java/com/example/ultimatemovieapp/MainActivity.kt
+++ app/src/main/java/com/example/ultimatemovieapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ultimatemovieapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,14 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var texto:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val texto = findViewById<TextView>(R.id.txt_output)
+        texto = findViewById<TextView>(R.id.txt_output)
         val boton = findViewById<Button>(R.id.btn_accion)
 
         boton.setOnClickListener {
@@ -21,6 +25,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun navegarSegundaActivity(){
         val intent = Intent(this,SegundaActivity::class.java)
-        startActivity(intent)
+        intent.putExtra("nombre","Curso Android")
+        startActivityForResult(intent,1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1){
+            if (resultCode == Activity.RESULT_OK){
+                val nombre = data?.getStringExtra("nombre2")
+                texto.text = nombre
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ultimatemovieapp/SegundaActivity.kt
+++ app/src/main/java/com/example/ultimatemovieapp/SegundaActivity.kt
@@ -1,16 +1,33 @@
 package com.example.ultimatemovieapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SegundaActivity : AppCompatActivity() {
+
+    private var nombre: String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
 
         val button = findViewById<Button>(R.id.btn_accion)
+        val texto = findViewById<TextView>(R.id.txt_output)
+
+        intent.extras?.let {bundle ->
+            nombre = bundle.getString("nombre")
+        }
+
+        texto.text = nombre
+
         button.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("nombre2","Hola desde segunda activity")
+            setResult(Activity.RESULT_OK,intent)
             finish()
         }
     }
